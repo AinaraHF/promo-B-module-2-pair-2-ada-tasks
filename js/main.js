00cbe35575bed9222cbe34ca905124815f4e41ac
Ana Elisa <elisaibz92@gmail.com>
2025-01-08 11:29:41 +0100

    metodos-funcionales-de-array

--- js/main.js
+++ js/main.js
@@ -13,11 +13,44 @@ const tasks = [
     taskList.innerHTML = '';
     for(const task of tasks){
     if (task.completed === true){
-        taskList.innerHTML += `<li class="tachado">${task.name}</li>`;
+        taskList.innerHTML += `<li class="tachado"><input id="${task.id}" type="checkbox" checked/>${task.name}</li>`;
     }else{
-        taskList.innerHTML += `<li>${task.name}</li>`;
+        taskList.innerHTML += `<li><input id="${task.id}" type="checkbox"/>${task.name}</li>`;
     }
     }
   };
 
   renderTasks();
+
+
+//obtener el id del checkboc clickado con finindex
+//buscar en que posicion se encuentra la tarea con ese id
+//cambiar dentro del array con valor completed al valor contrario
+
+const handleClickList = (event) => {
+  const taskId = parseInt(event.target.id); // Obtengo el id del checkbox clickado por la usuaria
+  if (!taskId) return; // Si no ha pulsado en el checkbox, no queremos hacer nada y salimos de la función
+  const doneTask = tasks.findIndex((task)=> task.id === taskId);
+  console.log(doneTask);
+  
+  if(tasks[doneTask].completed === true){
+    tasks[doneTask].completed = false;
+  }else {
+    tasks[doneTask].completed = true;
+
+  }
+  
+ 
+  renderTasks();
+  // Busca la tarea que tenga el id `taskId` en el array `tasks`
+  // Una vez que has obtenido la tarea, actualiza la propiedad `completed`
+  // Pinta de nuevo las tareas en el html
+};
+
+
+
+taskList.addEventListener("click", handleClickList);
+
+
+
+
