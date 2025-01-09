48140423074898f84a309fccfd6a7b69d9d30f16
AinaraHF <ainarahf@gmail.com>
2025-01-09 11:21:47 +0100

    peticion al servidor 1.1 hecho, 1.2 pendiente

--- js/main.js
+++ js/main.js
@@ -1,17 +1,32 @@
 'use strict';
 
 const taskList = document.querySelector('.js-tasklist');
+const btnFilter = document.querySelector('.js-btn-filter');
+const inputFilter = document.querySelector('.js-text-task-filter');
 
-const tasks = [
+/*const tasks = [
     { name: "Recoger setas en el campo", completed: true, id: 1 },
     { name: "Comprar pilas", completed: true, id: 2 },
     { name: "Poner una lavadora de blancos", completed: true, id: 3 },
     { name: "Aprender cómo se realizan las peticiones al servidor en JavaScript", completed: false, id: 4,},
-  ];
+  ];*/
 
-  function renderTasks(){
+let tasks = [];
+const GITHUB_USER = "<tu_usuario_de_github_aqui>";
+const SERVER_URL = `https://dev.adalab.es/api/todo/${GITHUB_USER}`;
+
+fetch('https://dev.adalab.es/api/todo')
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    tasks = data.results;
+    renderTasks(tasks);
+  });
+  
+
+function renderTasks(array){
     taskList.innerHTML = '';
-    for(const task of tasks){
+    for(const task of array){
     if (task.completed === true){
         taskList.innerHTML += `<li class="tachado"><input id="${task.id}" type="checkbox" checked/>${task.name}</li>`;
     }else{
@@ -20,8 +35,7 @@ const tasks = [
     }
   };
 
-  renderTasks();
-
+renderTasks(tasks);
 
 //obtener el id del checkboc clickado con finindex
 //buscar en que posicion se encuentra la tarea con ese id
@@ -40,17 +54,20 @@ const handleClickList = (event) => {
 
   }
   
- 
-  renderTasks();
+  renderTasks(tasks);
   // Busca la tarea que tenga el id `taskId` en el array `tasks`
   // Una vez que has obtenido la tarea, actualiza la propiedad `completed`
   // Pinta de nuevo las tareas en el html
 };
 
+taskList.addEventListener('click', handleClickList);
 
 
-taskList.addEventListener("click", handleClickList);
+function handleClickFilter(ev){
+  ev.preventDefault();
+    const inputValue = inputFilter.value;
+    const filteredValue = tasks.filter((task)=> task.name.includes(inputValue));
+    renderTasks(filteredValue);
+};
 
-
-
-
+btnFilter.addEventListener('click', handleClickFilter);
